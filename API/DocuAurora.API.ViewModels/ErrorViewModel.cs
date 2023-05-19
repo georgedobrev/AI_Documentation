691048d8c270a73ffec0fed96597e9ff1a3e1011
Milcho Kasmetov <milcho.kasmetov@blankfactor.com>
2023-05-19 17:06:12 +0300

    DocuAurora.API.ViewModels folder deletion

--- API/DocuAurora.API.ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DocuAurora.API.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
