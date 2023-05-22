e825255bbdda09db4e0aaf6329b45ee59b04b043
Milcho Kasmetov <milcho.kasmetov@blankfactor.com>
2023-05-22 14:29:40 +0300

    Adding Global constants for roles.

---
+++ Data/DocuAurora.Data/Seeding/UserSeeder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuAurora.Data.Seeding
+{
+    public class UserSeeder : ISeeder
+    {
+    }
+}
--- DocuAurora.Common/GlobalConstants.cs
+++ DocuAurora.Common/GlobalConstants.cs
@@ -4,6 +4,23 @@
     {
         public const string SystemName = "DocuAurora";
 
-        public const string AdministratorRoleName = "Administrator";
+        public const string AdministratorRoleName = "Admin";
+
+        public const string AdministratorEmail = "milcho.kasmetov@blankfactor.com";
+
+        public const string AdministratorPassword = "AdminskataParola123";
+
+        public const string UserRoleName = "User";
+
+        public const string UserEmail = "vladislav.milchov@blankfactor.com";
+
+        public const string UserPassword = "Guest333@";
+
+        public const string TrainerRoleName = "Trainer";
+
+        public const string TrainerEmail = "bozhidar.genchev@blankfactor.com";
+
+        public const string TrainerPassword = "Guest333@";
+      
     }
 }
