d1a234ce12b6b85c8439bb2dcf88522da08a0dff
Kasmetov <milcho.kasmetov@blankfactor.com>
2023-05-23 09:43:04 +0300

    Revert "DocuAurora.API.ViewModels folder deletion"
    This reverts commit 691048d8c270a73ffec0fed96597e9ff1a3e1011.

---
+++ API/DocuAurora.API.ViewModels/Administration/Dashboard/IndexViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DocuAurora.API.ViewModels.Administration.Dashboard
+{
+    public class IndexViewModel
+    {
+        public int SettingsCount { get; set; }
+    }
+}
---
+++ API/DocuAurora.API.ViewModels/Settings/SettingsListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DocuAurora.API.ViewModels.Settings
+{
+    using System.Collections.Generic;
+
+    public class SettingsListViewModel
+    {
+        public IEnumerable<SettingViewModel> Settings { get; set; }
+    }
+}
