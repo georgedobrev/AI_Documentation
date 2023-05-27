6e68c42aa7c3df45a0dca2c59aa22eeaaacacce5
Milcho Kasmetov <131446995+Kasmetov@users.noreply.github.com>
2023-05-27 18:25:34 +0300

    LogUserNameMiddleWare and SERILOG QUICK FIX
    Added middleWare and release of recourses from seriLog at end of the program.

---
+++ API/DocuAurora.API.Infrastructure/LogUserNameMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serilog.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuAurora.API.Infrastructure
+{
+    public class LogUserNameMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public LogUserNameMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            LogContext.PushProperty("UserName", context.User.Identity.Name);
+
+            return next(context);
+        }
+    }
+}
--- API/DocuAurora.API/Program.cs
+++ API/DocuAurora.API/Program.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Reflection;
     using DocuAurora.API;
+    using DocuAurora.API.Infrastructure;
     using DocuAurora.Data;
     using DocuAurora.Data.Common;
     using DocuAurora.Data.Common.Repositories;
@@ -41,6 +42,9 @@
             var app = builder.Build();
             Configure(app);
             app.Run();
+
+            Log.CloseAndFlush();
+
         }
 
         private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
@@ -116,6 +120,8 @@
             app.UseRouting();
 
             app.UseAuthentication();
+            // SERILOG USARNAME INSERTION
+            app.UseMiddleware<LogUserNameMiddleware>();
             app.UseAuthorization();
 
             app.MapControllerRoute("areaRoute", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
