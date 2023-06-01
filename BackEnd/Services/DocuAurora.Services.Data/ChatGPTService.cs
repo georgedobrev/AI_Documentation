4a2254fb2bd7e6d452ce1fbee15d90f4d753b311
Milcho Kasmetov <milcho.kasmetov@blankfactor.com>
2023-06-01 20:55:09 +0300

    ChatGPT controller logic converted into service

---
+++ BackEnd/Services/DocuAurora.Services.Data/ChatGPTService.cs
@@ -0,0 +1,43 @@
+﻿using DocuAurora.Services.Data.Contracts;
+using Microsoft.Extensions.Configuration;
+using OpenAI_API;
+using OpenAI_API.Completions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuAurora.Services.Data
+{
+    public class ChatGPTService : IChatGPTService
+    {
+        private readonly IConfiguration _configuration;
+
+        public ChatGPTService(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+
+        public async Task<string> GenerateResponseChatGPT(string inputText)
+        {
+            OpenAIAPI api = new OpenAIAPI(this._configuration["ChatGPTAPIkey"]);
+
+            var chat = api.Chat.CreateConversation();
+
+            CompletionRequest completionRequest = new CompletionRequest();
+            completionRequest.Prompt = inputText;
+            completionRequest.Model = OpenAI_API.Models.Model.ChatGPTTurbo;
+
+            /// give instruction as System
+            chat.AppendSystemMessage("You are a writer.");
+
+            chat.AppendUserInput(string.Format(this._configuration["ChatGPTtemplate"], inputText));
+
+            string response = await chat.GetResponseFromChatbotAsync();
+
+            return response;
+        }
+    }
+}
