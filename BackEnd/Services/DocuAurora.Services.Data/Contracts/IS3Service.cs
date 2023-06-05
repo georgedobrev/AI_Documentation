2e3e9f1353a91209be33e71d792d9b58c603a281
Milcho Kasmetov <milcho.kasmetov@blankfactor.com>
2023-06-05 15:16:05 +0300

    Interface created for AWS S3

--- BackEnd/Services/DocuAurora.Services.Data/Contracts/IS3Service.cs
+++ BackEnd/Services/DocuAurora.Services.Data/Contracts/IS3Service.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.IO;
 using System.Threading.Tasks;
 
 namespace DocuAurora.Services.Data.Contracts
 {
-	public interface IS3Service
-	{
+    public interface IS3Service
+    {
         Task<Stream> GetFileAsync(string bucketName, string key);
 
+        Task<string> UploadFileAsync(string bucketName, IFormFile file);
+
+        Task<bool> DeleteFileAsync(string bucketName, string key);
     }
 }
-
