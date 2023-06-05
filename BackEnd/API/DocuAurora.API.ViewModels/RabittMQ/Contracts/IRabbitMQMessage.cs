83db8f00bbfda6a3a4119fe131ea722c9e1301bb
Milcho Kasmetov <milcho.kasmetov@blankfactor.com>
2023-06-05 14:55:48 +0300

    RabbitMQMessage model creation

---
+++ BackEnd/API/DocuAurora.API.ViewModels/RabittMQ/Contracts/IRabbitMQMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuAurora.API.ViewModels
+{
+    public interface IRabbitMQMessage<T>
+    {
+        public string CommandName { get; set; }
+
+        public T Payload { get; set; }
+    }
+}
