d5754683014a5f21cb833c0bd065cf4f3c042cb1
Kasmetov <milcho.kasmetov@blankfactor.com>
2023-06-05 10:41:01 +0300

    Register SendGrid service

--- BackEnd/API/DocuAurora.API.Infrastructure/StartUpExtension.cs
+++ BackEnd/API/DocuAurora.API.Infrastructure/StartUpExtension.cs
@@ -62,10 +62,9 @@
             return services;
         }
 
-        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
+        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services, IConfiguration configuration)
         {
             // Application services
-            services.AddTransient<IEmailSender, NullMessageSender>();
             services.AddTransient<IEmailSender>(i =>
           new SendGridEmailSender(configuration.GetValue<string>("SendGridSettings:SendGridApiKey")));
             services.AddTransient<IAdminService, AdminService>();
@@ -278,7 +277,7 @@
             services.ConfigureDataRepositories();
 
             // Application services
-            services.ConfigureApplicationServices();
+            services.ConfigureApplicationServices(configuration);
 
             // Swagger configuration
             services.ConfigureSwagger();
