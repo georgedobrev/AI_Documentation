4ee854ad2a997508e523a882c9bcbe3b05697782
Kasmetov <milcho.kasmetov@blankfactor.com>
2023-06-07 12:22:04 +0300

    Encoding detection and sending it with payload

--- BackEnd/Services/DocuAurora.Services.Data/RabittMQService.cs
+++ BackEnd/Services/DocuAurora.Services.Data/RabittMQService.cs
@@ -13,6 +13,7 @@ using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Threading.Channels;
 using System.Threading.Tasks;
+using Ude;
 
 namespace DocuAurora.Services.Data
 {
@@ -27,15 +28,35 @@ namespace DocuAurora.Services.Data
 
         public void SendFile(IFormFile file, string queue, string exchange, string routingKey, IBasicProperties properties)
         {
+            var encoding = DetectEncoding(file);
+
             using (var memoryStream = new MemoryStream())
             {
                 file.CopyTo(memoryStream);
                 byte[] fileContent = memoryStream.ToArray();
-                this.channel.BasicPublish(exchange, routingKey, properties, body: fileContent);
+                string fb = string.Join(" ", fileContent);
+                var output = JsonSerializer.Serialize(new { encoding, fb});
+                this.channel.BasicPublish(exchange, routingKey, properties, Encoding.UTF8.GetBytes(output));
+
             }
         }
 
-        public void SendMessage<T>(T message, string queue, string exchange, string routingKey, IBasicProperties properties)
+        public string DetectEncoding(IFormFile file)
+        {
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                var detector = new CharsetDetector();
+                detector.Feed(reader.BaseStream);
+                detector.DataEnd();
+                if (detector.Charset != null)
+                {
+                    return detector.Charset;
+                }
+            }
+
+            return "Unknown";
+        }
+            public void SendMessage<T>(T message, string queue, string exchange, string routingKey, IBasicProperties properties)
         {
             var output = JsonSerializer.Serialize(message);
 
