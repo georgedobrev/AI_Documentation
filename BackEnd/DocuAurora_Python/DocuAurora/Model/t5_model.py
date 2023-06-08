174b6c319ca52af7b6c2d32b0591454fccb7bc5d
VladislavMilchov <131447185+VladislavMilchov@users.noreply.github.com>
2023-06-08 13:55:55 +0300

    fixing t5_model

--- BackEnd/DocuAurora_Python/DocuAurora/Model/t5_model.py
+++ BackEnd/DocuAurora_Python/DocuAurora/Model/t5_model.py
@@ -1,8 +1,6 @@
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-
-def generate_text(input_text):
-    model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
-    tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small")
-    inputs = tokenizer(input_text, return_tensors="pt")
-    outputs = model.generate(**inputs)
-    return tokenizer.batch_decode(outputs, skip_special_tokens=True)
+model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
+tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small")
+inputs = tokenizer("A step by step recipe to make bolognese pasta:", return_tensors="pt")
+outputs = model.generate(**inputs)
+print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
