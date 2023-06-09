d025ab3902d98417c0b66bd305ee03ff5493167f
Kasmetov <milcho.kasmetov@blankfactor.com>
2023-06-09 12:15:52 +0300

    Update main.py

--- BackEnd/DocuAurora_Python/DocuAurora/main.py
+++ BackEnd/DocuAurora_Python/DocuAurora/main.py
@@ -1,15 +1,10 @@
-
 from flask import Flask
 from Services.RabbitMQService import RabbitMQService
 from config import host_name, exchange_name, queue_name, message_routing_key, file_routing_key
 
 app = Flask(__name__)
 
-
-
 if __name__ == '__main__':
-
-
     rabbitmq_service = RabbitMQService(host_name, exchange_name, queue_name, message_routing_key, file_routing_key)
     rabbitmq_service.connect()
     rabbitmq_service.start_consuming()
