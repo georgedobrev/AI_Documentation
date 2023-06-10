a4218b15ef754069e89fc6e3508bf68ff62b84f7
Milcho Kasmetov <milcho.kasmetov@blankfactor.com>
2023-06-10 18:22:40 +0300

    Code optimization

--- BackEnd/DocuAurora_Python/DocuAurora/Services/DocumentStorageService.py
+++ BackEnd/DocuAurora_Python/DocuAurora/Services/DocumentStorageService.py
@@ -1,29 +1,38 @@
 import pinecone
 import configparser
 
+
 class DocumentStorageService:
     def __init__(self, index_name, config_file):
         self.config_file = config_file
         self.index_name = index_name
-        self.dimension =  self._get_config_value('Pinecone', 'dimension')
+        self.api_key = self._get_config_value('Pinecone', 'API_KEY')
+        self.dimension = self._get_config_value('Pinecone', 'dimension')
 
-    def _get_api_key(self, section, key):
-            config = configparser.ConfigParser()
-            config.read(self.config_file)
-            return config.get(section, key)
+    def _get_config_value(self, section, key):
+        config = configparser.ConfigParser()
+        config.read(self.config_file)
+        return config.get(section, key)
+
+    def _initialize_pinecone(self):
+        pinecone.init(api_key=self.api_key,environment=self._get_config_value('Pinecone', 'environment'))
+
+    def _deinitialize_pinecone(self):
+        pinecone.deinit()
 
     def create_index(self):
-        pinecone.init(api_key=self._get_api_key('Pinecone', 'API_KEY'))
+        self._initialize_pinecone()
         pinecone.create_index(index_name=self.index_name, dimension=self.dimension)
+        self._deinitialize_pinecone()
 
     def insert_document(self, document_id, embedding):
-        pinecone.init(api_key=self._get_api_key())
+        self._initialize_pinecone()
         index = pinecone.Index(index_name=self.index_name)
         index.upsert(items={document_id: embedding})
-        pinecone.deinit()
+        self._deinitialize_pinecone()
 
     def delete_document(self, document_id):
-        pinecone.init(api_key=self._get_api_key())
+        self._initialize_pinecone()
         index = pinecone.Index(index_name=self.index_name)
         index.delete(ids=[document_id])
-        pinecone.deinit()
+        self._deinitialize_pinecone()
