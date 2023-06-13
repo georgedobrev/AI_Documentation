14a3707b028d3d87a936945dbe59654f53c1686b
Kasmetov <milcho.kasmetov@blankfactor.com>
2023-06-13 12:28:06 +0300

    Code refactor

--- BackEnd/DocuAurora_Python/DocuAurora/Services/RabbitMQService.py
+++ BackEnd/DocuAurora_Python/DocuAurora/Services/RabbitMQService.py
@@ -3,47 +3,39 @@ import configparser
 import json
 
 class RabbitMQService:
-    def __init__(self, config_file):
-        self.config_file = config_file
-        self.host_name ='host_name'
-        self.exchange_name = 'exchange_name'
-
-        # to do change config file
-        self.message_queue_name = "DocuAurora-MessageT-Queue"
-        self.file_queue_name = "DocuAurora-FileT-Queue"
-        self.message_routing_key ='message_routing_key'
-        self.file_routing_key = 'RabbitMQ', 'file_routing_key'
+    def __init__(self, host='localhost', username='guest', password='guest'):
+        self.host = host
+        self.username = username
+        self.password = password
         self.connection = None
         self.channel = None
 
-    def _get_config_value(self, section, key):
-        config = configparser.ConfigParser()
-        config.read(self.config_file)
-        return config.get(section, key)
-
     def connect(self):
-        parameters = pika.URLParameters('amqp://guest:guest@localhost:5672/%2F')
-        self.connection = pika.SelectConnection(parameters=parameters, on_open_callback=self.on_connection_open)
-
-
-    def on_connection_open(self, connection):
+        credentials = pika.PlainCredentials(self.username, self.password)
+        parameters = pika.ConnectionParameters(self.host, credentials=credentials)
+        self.connection = pika.BlockingConnection(parameters)
         self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='direct')
-        self.channel.queue_declare(queue=self.message_queue_name)
-        self.channel.queue_declare(queue=self.file_queue_name)
-        self.channel.queue_bind(exchange=self.exchange_name, queue=self.message_queue_name,
-                                routing_key=self.file_routing_key)
-        self.channel.queue_bind(exchange=self.exchange_name, queue=self.file_queue_name,
-                                routing_key=self.message_routing_key)
 
-    def start_consuming(self, queue_name, callback):
+    def create_queues(self, queue1_name, queue2_name):
+        self.channel.queue_declare(queue=queue1_name)
+        self.channel.queue_declare(queue=queue2_name)
 
-        self.channel.basic_consume(
-            queue=queue_name,
-            on_message_callback=callback,
-            auto_ack=True
-        )
-
-        print(f' [*] {queue_name} waiting for messages. To exit, press CTRL+C')
-
+    def consume_messages(self, queue_name, callback):
+        self.channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
         self.channel.start_consuming()
+
+    def close_connection(self):
+        if self.connection and not self.connection.is_closed:
+            self.connection.close()
+
+    # Message callback function for queue 1
+
+
+def callback_queue1(channel, method, properties, body):
+    print(f"Received message from Queue 1: {body.decode()}")
+
+    # Message callback function for queue 2
+
+
+def callback_queue2(channel, method, properties, body):
+    print(f"Received message from Queue 2: {body.decode()}")
--- BackEnd/DocuAurora_Python/DocuAurora/main.py
+++ BackEnd/DocuAurora_Python/DocuAurora/main.py
@@ -9,7 +9,7 @@ from Services.RabbitMQMessageConsume import callbackMessage
 from Services.RabbitMQFileConsume import callbackFile
 
 
-from Model.t5_model import local_llm
+# from Model.t5_model import local_llm
 
 app = Flask(__name__)
 
@@ -18,11 +18,18 @@ if __name__ == '__main__':
 
     # Construct the path to the config file relative to the script's location
     config_file = script_dir / "config.ini"
-    rabbitmq_service = RabbitMQService(config_file)
+    rabbitmq_service = RabbitMQService()
     rabbitmq_service.connect()
-    #TO put right queue from config file
-    rabbitmq_service.start_consuming("DocuAurora-MessageT-Queue", callback=callbackMessage)
-    rabbitmq_service.start_consuming("DocuAurora-FileT-Queue", callback=callbackFile)
+    rabbitmq_service.create_queues('queue1', 'queue2')
+
+    # Start consuming messages from queue 1
+    rabbitmq_service.consume_messages('queue1', callbackMessage)
+
+    # Start consuming messages from queue 2
+    rabbitmq_service.consume_messages('queue2', callbackFile)
+
+    # Close the connection
+    # rabbitmq_service.close_connection()
 
     # TO DO put this logic in rabittmq when receive msg to send file to model and import s3SERVICE !!!
     # s3_service = S3Service(region_name)
@@ -33,7 +40,7 @@ if __name__ == '__main__':
     # document_text = "Your document text here"
     # embedding = embedding_service.get_document_embedding(document_text)
     # print(embedding)
-    print(local_llm('What is the capital of France')) # to do input message
+    # print(local_llm('What is the capital of France')) # to do input message
     app.run(host='0.0.0.0', port=5000, debug=True)
 
 
