bb90df16436137a6b5e1b5007d85066ad125e974
VladislavMilchov <131447185+VladislavMilchov@users.noreply.github.com>
2023-06-13 20:34:38 +0300

    setting up pinecone and pdf loader

--- BackEnd/DocuAurora_Python/DocuAurora/Resources/pdf_loader.py
+++ BackEnd/DocuAurora_Python/DocuAurora/Resources/pdf_loader.py
@@ -1,6 +1,6 @@
 from langchain.document_loaders import DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings. import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores.pinecone import Pinecone
 from langchain.document_loaders import PyPDFLoader
 
@@ -12,35 +12,18 @@ PINECONE_INDEX_NAME = "<Your_Pinecone_Index_Name>"
 PINECONE_NAME_SPACE = "<Your_Pinecone_Namespace>"
 
 async def run():
-    try:
-        # Load raw documents from all files in the directory
-        file_path = Path("docs")
-        directory_loader = DirectoryLoader(file_path, {".pdf": PyPDFLoader})
-
-        raw_docs = await directory_loader.load()
+    
+    # Load raw documents from all files in the directory
+   
+        loader = PyPDFLoader("your_document.pdf")
+        documents = loader.load_and_split()
 
         # Split text into chunks
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 
-        docs = await text_splitter.split_documents(raw_docs)
-        print('Split docs:', docs)
+        chunks = text_splitter.split_documents(documents)
 
-        print('Creating vector store...')
-        # Create and store the embeddings in the vectorStore
-        embeddings = OpenAIEmbeddings()
+        embeddings = HuggingFaceEmbeddings(model, tokenizer)
 
-        index = pinecone.Index(PINECONE_INDEX_NAME)
 
-        # Embed the PDF documents
-        await Pinecone.from_documents(docs, embeddings, {
-            "pinecone_index": index,
-            "namespace": PINECONE_NAME_SPACE,
-            "text_key": "text",
-        })
-    except Exception as error:
-        print('Error:', error)
-        raise Exception('Failed to ingest your data')
 
-# Run the script
-asyncio.run(run())
-print('Ingestion complete.')
