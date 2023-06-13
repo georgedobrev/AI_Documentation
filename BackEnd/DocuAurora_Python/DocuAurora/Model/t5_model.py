6dc06ab82a6213c42e1a5c7dca9e266dbf6e57ee
VladislavMilchov <131447185+VladislavMilchov@users.noreply.github.com>
2023-06-13 14:21:05 +0300

    setting up embeddings and pdf loader

--- BackEnd/DocuAurora_Python/DocuAurora/Model/t5_model.py
+++ BackEnd/DocuAurora_Python/DocuAurora/Model/t5_model.py
@@ -1,6 +1,8 @@
 from langchain.llms import HuggingFacePipeline
 import torch
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, AutoModelForCausalLM, pipeline
+from langchain.embeddings import HuggingFaceEmbeddings
+
 model_id = 'google/flan-t5-base'
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
@@ -14,3 +16,24 @@ pipe = pipeline(
 local_llm = HuggingFacePipeline(pipeline=pipe)
 
 print(local_llm('translate English to German:How old are you'))
+
+class T5Embeddings(HuggingFaceEmbeddings):
+    def __init__(self, model_id):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+
+    async def transform_documents(self, documents):
+        results = []
+        for document in documents:
+            inputs = self.tokenizer.encode_plus(
+                document.page_content,
+                return_tensors="pt",
+                max_length=512,
+                truncation=True,
+            )
+            with torch.no_grad():
+                outputs = self.model(**inputs)
+            # Use the mean of the last hidden state to represent the document
+            embedding = outputs.last_hidden_state.mean(dim=1).detach().numpy()
+            results.append((document.metadata['start_index'], embedding))
+        return results
--- BackEnd/DocuAurora_Python/DocuAurora/Resources/pdf_loader.py
+++ BackEnd/DocuAurora_Python/DocuAurora/Resources/pdf_loader.py
@@ -1,12 +1,46 @@
-from flask import request
-from flask_restful import Resource
+from langchain.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings. import OpenAIEmbeddings
+from langchain.vectorstores.pinecone import Pinecone
+from langchain.document_loaders import PyPDFLoader
 
-class TextProcessor(Resource):
-    def get(self):
-        return {'message': 'Hello team'}
+from pathlib import Path
+import pinecone
+import asyncio
 
-    def post(self):
-        data = request.get_json()
-        text = data['hello team']
-        print(text)
-        return {'message': 'Text processed successfully'}, 200
+PINECONE_INDEX_NAME = "<Your_Pinecone_Index_Name>"
+PINECONE_NAME_SPACE = "<Your_Pinecone_Namespace>"
+
+async def run():
+    try:
+        # Load raw documents from all files in the directory
+        file_path = Path("docs")
+        directory_loader = DirectoryLoader(file_path, {".pdf": PyPDFLoader})
+
+        raw_docs = await directory_loader.load()
+
+        # Split text into chunks
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
+        docs = await text_splitter.split_documents(raw_docs)
+        print('Split docs:', docs)
+
+        print('Creating vector store...')
+        # Create and store the embeddings in the vectorStore
+        embeddings = OpenAIEmbeddings()
+
+        index = pinecone.Index(PINECONE_INDEX_NAME)
+
+        # Embed the PDF documents
+        await Pinecone.from_documents(docs, embeddings, {
+            "pinecone_index": index,
+            "namespace": PINECONE_NAME_SPACE,
+            "text_key": "text",
+        })
+    except Exception as error:
+        print('Error:', error)
+        raise Exception('Failed to ingest your data')
+
+# Run the script
+asyncio.run(run())
+print('Ingestion complete.')
