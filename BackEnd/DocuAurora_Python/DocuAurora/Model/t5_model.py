ae71c85384b9f857e6d0c8b76ff9aa3f56ecc2bc
VladislavMilchov <131447185+VladislavMilchov@users.noreply.github.com>
2023-06-17 00:49:55 +0300

    making methods

--- BackEnd/DocuAurora_Python/DocuAurora/Model/t5_model.py
+++ BackEnd/DocuAurora_Python/DocuAurora/Model/t5_model.py
@@ -1,56 +1,48 @@
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
 from langchain.document_loaders import PyPDFLoader
-from langchain.document_loaders import TextLoader
-from langchain.document_loaders import DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores.pinecone import Pinecone
 from langchain.llms import HuggingFacePipeline
-from langchain import PromptTemplate,  LLMChain
+from langchain.vectorstores.pinecone import Pinecone
 from langchain.chains import RetrievalQA
 import pinecone
-import textwrap
 
-def process_llm_response(llm_response):
-    print(llm_response['result'])
-    print('\n\nSources:')
-    for source in llm_response["source_documents"]:
-        print(source.metadata['source'])
+def setup_model(model_id):
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+    pipe = pipeline("text2text-generation", model=model, tokenizer=tokenizer)
+    return HuggingFacePipeline(pipeline=pipe)
 
-model_id = 'google/flan-t5-base'
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+def load_documents(pdf_path):
+    loader = PyPDFLoader(pdf_path)
+    return loader.load_and_split()
 
-pipe = pipeline(
-    "text2text-generation",
-    model=model,
-    tokenizer=tokenizer
-)
+def split_text(documents, chunk_size=1000, chunk_overlap=200):
+    text_splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    return text_splitter.split_documents(documents)
 
-local_llm = HuggingFacePipeline(pipeline=pipe)
+def setup_pinecone(chunks, model_name, api_key, environment, index_name):
+    embeddings = HuggingFaceInstructEmbeddings(model_name=model_name, model_kwargs={"device": "cpu"})
+    pinecone.init(api_key=api_key, environment=environment)
+    pineconedb = Pinecone.from_documents(documents=chunks, embedding=embeddings, index_name=index_name)
+    return pineconedb.as_retriever(search_kwargs={"k": 1})
+
+def setup_retrieval_qa(local_llm, retriever):
+    return RetrievalQA.from_chain_type(llm=local_llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+
+def ask_question(qa_chain, query):
+    def process_llm_response(llm_response):
+        print(llm_response['result'])
+        print('\n\nSources:')
+        for source in llm_response["source_documents"]:
+            print(source.metadata['source'])
+    llm_response = qa_chain(query)
+    process_llm_response(llm_response)
 
 
-loader = PyPDFLoader('C:/C# - learning/AI_Documentation/BackEnd/DocuAurora_Python/DocuAurora/Model/Bulgaria.pdf')
+local_llm = setup_model('google/flan-t5-base')
+retriever = setup_pinecone( "hkunlp/instructor-xl", 'b4b7947c-96fd-4c95-9785-9c8ced03b64b', 'us-west1-gcp-free', "test2")
+qa_chain = setup_retrieval_qa(local_llm, retriever)
 
-documents = loader.load_and_split()
-
-# Split text into chunks
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-chunks = text_splitter.split_documents(documents)
-
-embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl", model_kwargs={"device": "cpu"})
-
-pinecone.init(api_key='b4b7947c-96fd-4c95-9785-9c8ced03b64b', environment='us-west1-gcp-free')
-
-index_name = "test2"  # replace with your index name
-pineconedb = Pinecone.from_documents(documents=chunks, embedding=embeddings, index_name=index_name)
-
-retriever = pineconedb.as_retriever(search_kwargs={"k": 3})
-
-qa_chain = RetrievalQA.from_chain_type(llm=local_llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
-
-query = "what is the capital of Germany? "
-llm_response = qa_chain(query)
-process_llm_response(llm_response)
-
+# Asking Questions
+ask_question(qa_chain, "who have contribution for book history in Bulgaria")
