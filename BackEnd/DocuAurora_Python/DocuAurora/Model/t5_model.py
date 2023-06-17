c147ec3ad5c58f9aab5a643c40bab1ce22c3ab33
VladislavMilchov <131447185+VladislavMilchov@users.noreply.github.com>
2023-06-17 17:44:26 +0300

    Setting model for 2 documents

--- BackEnd/DocuAurora_Python/DocuAurora/Model/t5_model.py
+++ BackEnd/DocuAurora_Python/DocuAurora/Model/t5_model.py
@@ -25,7 +25,7 @@ def setup_pinecone(chunks, model_name, api_key, environment, index_name):
     embeddings = HuggingFaceInstructEmbeddings(model_name=model_name, model_kwargs={"device": "cpu"})
     pinecone.init(api_key=api_key, environment=environment)
     pineconedb = Pinecone.from_documents(documents=chunks, embedding=embeddings, index_name=index_name)
-    return pineconedb.as_retriever(search_kwargs={"k": 1})
+    return pineconedb.as_retriever(search_kwargs={"k": 2})
 
 def setup_retrieval_qa(local_llm, retriever):
     return RetrievalQA.from_chain_type(llm=local_llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
@@ -41,8 +41,13 @@ def ask_question(qa_chain, query):
 
 
 local_llm = setup_model('google/flan-t5-base')
-retriever = setup_pinecone( "hkunlp/instructor-xl", 'b4b7947c-96fd-4c95-9785-9c8ced03b64b', 'us-west1-gcp-free', "test2")
+documents = load_documents('C:/C# - learning/AI_Documentation/BackEnd/DocuAurora_Python/DocuAurora/Model/Bulgaria.pdf')
+documents = load_documents('C:/C# - learning/AI_Documentation/BackEnd/DocuAurora_Python/DocuAurora/Model/Test2.pdf')
+chunks = split_text(documents)
+retriever = setup_pinecone(chunks, "hkunlp/instructor-xl", 'b4b7947c-96fd-4c95-9785-9c8ced03b64b', 'us-west1-gcp-free', "test2")
 qa_chain = setup_retrieval_qa(local_llm, retriever)
 
 # Asking Questions
-ask_question(qa_chain, "who have contribution for book history in Bulgaria")
+ask_question(qa_chain, "who have contribution for book history in Bulgaria?")
+ask_question(qa_chain, "who is John Draper?")
+ask_question(qa_chain, "what is the capital of Germany?")
