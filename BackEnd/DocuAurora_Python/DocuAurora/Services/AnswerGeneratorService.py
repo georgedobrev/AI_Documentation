8e41911fafa16e2039a4e4ffdeee77cc48e0d702
Milcho Kasmetov <milcho.kasmetov@blankfactor.com>
2023-06-19 19:30:03 +0300

    Created AnswerGeneratorService

---
+++ BackEnd/DocuAurora_Python/DocuAurora/Services/AnswerGeneratorService.py
@@ -0,0 +1,36 @@
+from Model.t5_model import setup_model, load_documents, setup_pinecone, setup_retrieval_qa, split_text, ask_question
+import configparser
+
+
+class AnswerGeneratorService:
+    def __init__(self, config_file):
+        self.config_file = config_file
+        self.local_llm = setup_model('google/flan-t5-base')
+        self.documents = []
+        self.chunks = []
+        self.model_namePinecone = self._get_config_value('Pinecone', 'model_name')
+        self.api_key = self._get_config_value('Pinecone', 'API_KEY')
+        self.environment = self._get_config_value('Pinecone', 'environment')
+        self.index_name = self._get_config_value('Pinecone', 'environment')
+        self.retriever = None
+        self.qa_chain = None
+
+    def _get_config_value(self, section, key):
+        config = configparser.ConfigParser()
+        config.read(self.config_file)
+        return config.get(section, key)
+
+    def load_documents(self, document_paths):
+        for path in document_paths:
+            document = load_documents(path)
+            self.documents.append(document)
+        self.chunks = split_text(self.documents)
+        self.retriever = setup_pinecone(self.chunks,
+                                        self.model_namePinecone,
+                                        self.api_key,
+                                        self.environment,
+                                        self.index_name)
+        self.qa_chain = setup_retrieval_qa(self.local_llm, self.retriever)
+
+    def generate_answer(self, question):
+        return ask_question(self.qa_chain, question)
--- BackEnd/DocuAurora_Python/DocuAurora/main.py
+++ BackEnd/DocuAurora_Python/DocuAurora/main.py
@@ -34,21 +34,10 @@ if __name__ == '__main__':
     rabbitmq_service.create_queues()
     rabbitmq_service.consume_messages(queue_callbacks)
 
-    # Close the connection
-    # rabbitmq_service.close_connection()
-
     # TO DO put this logic in rabittmq when receive msg to send file to model and import s3SERVICE !!!
     # s3_service = S3Service(region_name)
     # s3_service.process_files(bucket_name, object_keys, program_path)
 
-    # TO DO connect this logic T5 tokenizer
-    # embedding_service = T5EmbeddingService()
-    # document_text = "Your document text here"
-    # embedding = embedding_service.get_document_embedding(document_text)
-    # print(embedding)
-    # var ime = local_llm('What is the capital of France')
-    # var ime
-    # print(local_llm('What is the capital of France')) # to do input message
 
     local_llm = setup_model('google/flan-t5-base')
     documents = load_documents('C:/C# - learning/AI_Documentation/BackEnd/DocuAurora_Python/DocuAurora/Model/Bulgaria.pdf')
