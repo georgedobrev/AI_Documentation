40e3a2dbf80d4459645e34572196c4dc86b4cc3a
kolevgenchevwork <bozhidar.genchev@blankfactor.com>
2023-07-10 09:51:47 +0300

    required updates

--- FrontEnd/AI_Documentation_FrontEnd/src/Components/Main/Sidebar.tsx
+++ FrontEnd/AI_Documentation_FrontEnd/src/Components/Main/Sidebar.tsx
@@ -3,25 +3,25 @@ import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@mui/materia
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 
+const sidebarItems = ['Inbox', 'Starred', 'Send email', 'Drafts'];
+
 const Sidebar: React.FC = () => {
   return (
-    <div>
-      <Drawer
-        variant="permanent"
-        anchor="left"
-      >
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-    </div>
+    <Drawer
+      variant="permanent"
+      anchor="left"
+    >
+      <List>
+        {sidebarItems.map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
   );
 }
 
